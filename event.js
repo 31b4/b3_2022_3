5f7e8b8fe155dd99b5be46a337b6f3bd28c8a0fc
31b4 <75566095+31b4@users.noreply.github.com>
2022-11-02 16:53:10 +0100

    Add files via upload

--- event.js
+++ event.js
@@ -71,8 +71,8 @@ var falak = [
   ]
   //----------------------------------------
 var canvas, ctx,
-  playerX = 400,
-  playerY = 500,
+  playerX = 566,
+  playerY = 670,
   playerW = 20,
   playerH = 20,
   playerX2 = playerX+playerW,
@@ -81,19 +81,22 @@ var canvas, ctx,
   playerSpeedY = 0;
 var hang = new Image();
 hang.src = 'img/mic.png';
+var mozgas = new Image();
+mozgas.src = 'img/motion.png'
 
 //------------------------------onload-----------------------------------
 var fps = 60;
-var interval
+var interval;
 window.onload = function() {
   canvas = document.getElementById("jatekTer");
   ctx = canvas.getContext("2d");
-  Interval()
+  MozgasErzekeloLetrehozas();
+  Interval();
   
 }
 function Interval() {
   interval = setInterval(function() {
-    document.getElementById("h1").innerHTML=playerX +' ; '+ playerY
+    document.getElementById("h1").innerHTML=playerX +' ; '+ playerY;
     drawAll();
     playerMove();
     
@@ -130,16 +133,16 @@ var KEY_W = 87,
     keyHeld_Down = true;
   }
   if (evt.keyCode == KEY_SHIFT && !keyHeld_Space) {
-    fps = 120
-    keyHeld_Shift = true
-    clearInterval(interval)
-    Interval()
+    fps = 120;
+    keyHeld_Shift = true;
+    clearInterval(interval);
+    Interval();
   }
   if (evt.keyCode == KEY_SPACE && !keyHeld_Shift) {
-    fps = 40
-    keyHeld_Space = true
-    clearInterval(interval)
-    Interval()
+    fps = 40;
+    keyHeld_Space = true;
+    clearInterval(interval);
+    Interval();
   }
 
   evt.preventDefault();
@@ -166,16 +169,16 @@ function keyReleased(evt) {
     playerSpeedX = 0;
   }
   if (evt.keyCode == KEY_SHIFT) {
-    fps = 60
-    keyHeld_Shift = false
-    clearInterval(interval)
-    Interval()
+    fps = 60;
+    keyHeld_Shift = false;
+    clearInterval(interval);
+    Interval();
   }
   if (evt.keyCode == KEY_SPACE) {
-    fps = 60
-    keyHeld_Space = false
-    clearInterval(interval)
-    Interval()
+    fps = 60;
+    keyHeld_Space = false;
+    clearInterval(interval);
+    Interval();
   }
   
 }
@@ -193,31 +196,70 @@ function HangErzekeles() {
   else if ( keyHeld_Space){
     if (euklidesziTav(620,270,playerX+playerW/2,playerY+playerH/2)<=100) {
       ctx.beginPath();
-      ctx.strokeStyle="yellow"
+      ctx.strokeStyle="yellow";
       ctx.lineWidth = 3;
       ctx.arc(600+20, 250+20, 100, 0, 2 * Math.PI);
-      ctx.stroke()
+      ctx.stroke();
     } 
   }
   else{
     if (euklidesziTav(620,270,playerX+playerW/2,playerY+playerH/2)<=150) {
       ctx.beginPath();
-      ctx.strokeStyle="yellow"
+      ctx.strokeStyle="yellow";
       ctx.lineWidth = 3;
       ctx.arc(600+20, 250+20, 150, 0, 2 * Math.PI);
-      ctx.stroke()
+      ctx.stroke();
     } 
   }
 }
-function playerMove() {
-  playerX += playerSpeedX;
-  playerY += playerSpeedY;
+function MozgasErzekeles() {
+  let x1 = me_pontja[0]
+  let x2 = me_pontja[1]
+  for (let i = 0; i < mePontjai.length; i+=5) {
+    for (let k = playerY; k < playerY+playerH; k++) {
+        let y1 = mePontjai[i][0]
+        let y2 = mePontjai[i][1]
+        if (Math.round(euklidesziTav(x1,x2,y1,y2)) == Math.round(euklidesziTav(playerX,k,x1,x2))+Math.round(euklidesziTav(playerX,k,y1,y2))) {
+          var mozg = canvas.getContext("2d");
+          mozg.beginPath();
+          mozg.lineWidth = 1;
+          mozg.strokeStyle = 'yellow';
+          for (let i = 0; i < mePontjai.length; i+=5) {
+            mozg.moveTo(me_pontja[0],me_pontja[1])
+            mozg.lineTo(mePontjai[i][0], mePontjai[i][1])
+            mozg.stroke()
+          }
+        } 
+    }
+    /*
+    for (let k = playerX; k < playerX+ playerW; k++) {
+      let y1 = mePontjai[i][0]
+      let y2 = mePontjai[i][1]
+      if (euklidesziTav(x1,x2,y1,y2) >= euklidesziTav(k,playerY+playerH,x1,x2)+euklidesziTav(k,playerY+playerH,y1,y2) ) {
+        var mozg = canvas.getContext("2d");
+        mozg.beginPath();
+        mozg.lineWidth = 1;
+        mozg.strokeStyle = 'yellow';
+        for (let i = 0; i < mePontjai.length; i+=1) {
+          mozg.moveTo(me_pontja[0],me_pontja[1])
+          mozg.lineTo(mePontjai[i][0], mePontjai[i][1])
+          mozg.stroke()
+        }
+      } 
+    }*/
+  }
+}
 
+function playerMove() {
+    playerX += playerSpeedX;
+    playerY += playerSpeedY;
+  
     
     if (keyHeld_Up && !keyHeld_Down) {
-      HangErzekeles()
+      HangErzekeles();
+      MozgasErzekeles();
       playerSpeedY = -1;
-      checkEdge()
+      checkEdge();
       if (Utkozes(falak,falak_pontjai)[0]) {
         playerY -= playerSpeedY;
         playerSpeedY =0;
@@ -225,19 +267,24 @@ function playerMove() {
       
     }
     else if (keyHeld_Down && !keyHeld_Up) {
-      HangErzekeles()
+      HangErzekeles();
+      MozgasErzekeles();
       playerSpeedY = 1;
-      checkEdge()
+      checkEdge();
       if (Utkozes(falak,falak_pontjai)[0]) {
         playerY -= playerSpeedY;
         playerSpeedY =0;
       }
-      
+    }
+    else if(keyHeld_Down && keyHeld_Up){
+      playerY -= playerSpeedY;
+      playerSpeedY =0;
     }
     if (keyHeld_Left && !keyHeld_Right) {
-      HangErzekeles()
+      HangErzekeles();
+      MozgasErzekeles();
       playerSpeedX = -1;
-      checkEdge()
+      checkEdge();
       if (Utkozes(falak,falak_pontjai)[0]) {
         playerX -= playerSpeedX;
         playerSpeedX = 0;
@@ -245,14 +292,18 @@ function playerMove() {
       
     }
     else if (keyHeld_Right  && !keyHeld_Left) {
-      HangErzekeles()
+      HangErzekeles();
+      MozgasErzekeles();
       playerSpeedX = 1;
-      checkEdge()
+      checkEdge();
       if (Utkozes(falak,falak_pontjai)[0]) {
         playerX -= playerSpeedX;
         playerSpeedX = 0;
       }
-      
+    }
+    else if(keyHeld_Right  && keyHeld_Left){
+      playerX -= playerSpeedX;
+      playerSpeedX = 0;
     }
   
   //console.log("P1"+playerX+';'+playerY + "P2"+playerX2+';'+playerY2)
@@ -260,33 +311,33 @@ function playerMove() {
 }
 function checkEdge(){// ha kivul van a canvason visszadobja
   if (playerY <= 0) {
-    playerY = 0
+    playerY = 0;
   } else if (playerY >= canvas.height - playerH) { 
-    playerY = canvas.height - playerH
+    playerY = canvas.height - playerH;
   }
 
   if (playerX >= canvas.width - playerW) {
-    playerX = canvas.width - playerW
+    playerX = canvas.width - playerW;
   } else if (playerX <= 0) {
-    playerX = 0
+    playerX = 0;
   }
-  return true
+  return true;
 }
 //------------------------------DRAVING-----------------------------------
 function drawAll() {
   colorRect(0, 0, canvas.width, canvas.height, 'green');
-  colorRect(playerX, playerY, playerW, playerH, 'white');
-
+  
+  
+  var c = document.getElementById("jatekTer");
   
 
-  var c = document.getElementById("jatekTer");
   var ajto = c.getContext("2d");
   ajto.beginPath();
   ajto.lineWidth = 5;
   ajto.strokeStyle = 'red';
   for (let i = 0; i < ajtok.length; i++) {
-    ajto.moveTo(ajtok_pontjai[ajtok[i][0]][0],ajtok_pontjai[ajtok[i][0]][1])
-    ajto.lineTo(ajtok_pontjai[ajtok[i][1]][0],ajtok_pontjai[ajtok[i][1]][1])
+    ajto.moveTo(ajtok_pontjai[ajtok[i][0]][0],ajtok_pontjai[ajtok[i][0]][1]);
+    ajto.lineTo(ajtok_pontjai[ajtok[i][1]][0],ajtok_pontjai[ajtok[i][1]][1]);
     ajto.stroke();
   }
   var fal = c.getContext("2d");
@@ -295,8 +346,8 @@ function drawAll() {
   fal.lineWidth = 1;
   //falak
   for (let i = 0; i < falak.length; i++) {
-    fal.moveTo(falak_pontjai[falak[i][0]][0],falak_pontjai[falak[i][0]][1])
-    fal.lineTo(falak_pontjai[falak[i][1]][0],falak_pontjai[falak[i][1]][1])
+    fal.moveTo(falak_pontjai[falak[i][0]][0],falak_pontjai[falak[i][0]][1]);
+    fal.lineTo(falak_pontjai[falak[i][1]][0],falak_pontjai[falak[i][1]][1]);
     fal.stroke();
   }
   //ajtok
@@ -306,59 +357,73 @@ function drawAll() {
   ablak.strokeStyle ="lightblue"
   ablak.lineWidth = 2;
   for (let i = 0; i < ablakok.length; i+=2) {
-    ablak.moveTo(ablakok[i][0],ablakok[i][1])
-    ablak.lineTo(ablakok[i+1][0],ablakok[i+1][1])
-    ablak.stroke()
+    ablak.moveTo(ablakok[i][0],ablakok[i][1]);
+    ablak.lineTo(ablakok[i+1][0],ablakok[i+1][1]);
+    ablak.stroke();
   }
+  //mozgas szenzor
+  ctx.drawImage(mozgas, 37, 620,40,40);
   //hang szenzor 620,270
-  
-  ctx.drawImage(hang, 600, 250,40,40)
+  ctx.drawImage(hang, 600, 250,40,40);
   ctx.beginPath();
-  ctx.strokeStyle="red"
+  ctx.strokeStyle="red";
   ctx.lineWidth = 3;
   ctx.arc(600+20, 250+20, 100, 0, 2 * Math.PI);
-  ctx.stroke()
+  ctx.stroke();
   ctx.beginPath();
-  ctx.strokeStyle="red"
+  ctx.strokeStyle="red";
   ctx.lineWidth = 3;
   ctx.arc(600+20, 250+20, 150, 0, 2 * Math.PI);
-  ctx.stroke()
+  ctx.stroke();
   ctx.beginPath();
-  ctx.strokeStyle="red"
+  ctx.strokeStyle="red";
   ctx.lineWidth = 3;
   ctx.arc(600+20, 250+20, 230, 0, 2 * Math.PI);
-  ctx.stroke()
+  ctx.stroke();
   //erzekelo ajtok
   var xy = Utkozes(ajtok, ajtok_pontjai)
   if(xy[0]){//true / false
-      xy[1]
-      let x1 = xy[1][0]
-      let x2 = xy[1][1]
-      let y1 = xy[1][2]
-      let y2 = xy[1][3]
-      colorChange(x1,x2,y1,y2,"green")
+      xy[1];
+      let x1 = xy[1][0];
+      let x2 = xy[1][1];
+      let y1 = xy[1][2];
+      let y2 = xy[1][3];
+      colorChange(x1,x2,y1,y2,"yellow");
   
   }
+  //mozgerz
+  /*
+  var mozg = c.getContext("2d");
+  mozg.beginPath();
+  mozg.lineWidth = 1;
+  mozg.strokeStyle = 'red';
+  for (let i = 0; i < mePontjai.length; i+=1) {
+    mozg.moveTo(me_pontja[0],me_pontja[1]);
+    mozg.lineTo(mePontjai[i][0], mePontjai[i][1]);
+    mozg.stroke();
+  }*/
+  //player
+  colorRect(playerX, playerY, playerW, playerH, 'white');
 }
 
 
 function colorRect(leftX, topY, width, height, drawColor) {
-  ctx.beginPath()
+  ctx.beginPath();
   ctx.fillStyle = drawColor;
   ctx.fillRect(leftX, topY, width, height);
 }
 function colorChange(x1,x2,y1,y2,color){
   var c = document.getElementById("jatekTer");
   var atszin = c.getContext("2d");
-  atszin.beginPath()
-  atszin.moveTo(x1,x2)
-  atszin.lineTo(y1,y2)
+  atszin.beginPath();
+  atszin.moveTo(x1,x2);
+  atszin.lineTo(y1,y2);
   atszin.lineWidth = 5;
-  atszin.strokeStyle = 'yellow'
-  atszin.stroke()
+  atszin.strokeStyle = color;
+  atszin.stroke();
 }
 function colorText(shownText, xPos, yPos, drawColor) {
-  ctx.beginPath()
+  ctx.beginPath();
   ctx.fillStyle = drawColor;
   ctx.fillText(shownText, xPos, yPos);
 }
@@ -366,43 +431,111 @@ function colorText(shownText, xPos, yPos, drawColor) {
 
 function Utkozes(elem,elem_pontjai){
   for (let i = 0; i < elem.length; i++) {
-    var x1 = elem_pontjai[elem[i][0]][0] //p1
-    var x2= elem_pontjai[elem[i][0]][1] //p1
-    var y1 = elem_pontjai[elem[i][1]][0] //p2
-    var y2= elem_pontjai[elem[i][1]][1] //p2
+    var x1 = elem_pontjai[elem[i][0]][0]; //p1
+    var x2= elem_pontjai[elem[i][0]][1]; //p1
+    var y1 = elem_pontjai[elem[i][1]][0]; //p2
+    var y2= elem_pontjai[elem[i][1]][1];//p2
 
     //-----hatekonysag javitasat igenyli
     for (let i = playerX; i < playerX+ playerW; i++) {
       for (let j = playerY; j < playerY+ playerH; j++) {
-        if (euklidesziTav(x1,x2,y1,y2) >= euklidesziTav(i,j,x1,x2)+euklidesziTav(i,j,y1,y2)-0 && euklidesziTav(x1,x2,y1,y2) <= euklidesziTav(i,j,x1,x2)+euklidesziTav(i,j,y1,y2)+0) {
-          return [true,[x1,x2,y1,y2]]
+        if (euklidesziTav(x1,x2,y1,y2) >= euklidesziTav(i,j,x1,x2)+euklidesziTav(i,j,y1,y2) && euklidesziTav(x1,x2,y1,y2) <= euklidesziTav(i,j,x1,x2)+euklidesziTav(i,j,y1,y2)) {
+          return [true,[x1,x2,y1,y2]];
         } 
       }
     }
     
   }
-  return false
+  return false;
 }
 function euklidesziTav(x1,x2,y1,y2) {
-  return Math.sqrt(Math.pow(x1-y1,2)+Math.pow(x2-y2,2))
+  return Math.sqrt(Math.pow(x1-y1,2)+Math.pow(x2-y2,2));
 }
 
 
 
+//var me_pontja = [463,634]
+var me_pontja = [60,640];
+var mePontjai=[];
+function MozgasErzekeloLetrehozas() {
+  var falak_e = FalakEgyenesEgyenlete();
+  Abc(60,1150,30,falak_e);
+  Abc(30,640,1150,falak_e);
+}
+function Abc(n,m,k,falak_e){
+  var x1 = me_pontja[0]; // B
+  var y1 = me_pontja[1];
+  for (let i = n; i <= m; i+=10) {
+    let x2 = i; //A
+    let y2 = k;
+    if (k == 1150) {//----szar---------------------------
+      x2 = k;
+      y2 = i;
+    }
+    let irany_vektor = IranyVektor(x1,y1,x2,y2);
+    let normal_vektor = NormalVektor(irany_vektor);
+    var abc = EgyenesEgyenlete(normal_vektor,[x2,y2]);
+    var sv = [];
+    var sv2 = [];
+    for (let j = 0; j < falak_e.length; j++) {
+      var mecc =EgyenletRendszer(falak_e[j],abc);
+      if (mecc!=-1) {
+        if (euklidesziTav(x2,y2,x1,y1) >= euklidesziTav(x2,y2,mecc[0],mecc[1]) + euklidesziTav(mecc[0],mecc[1],x1,y1)-10 && euklidesziTav(x2,y2,x1,y1) <= euklidesziTav(x2,y2,mecc[0],mecc[1]) + euklidesziTav(mecc[0],mecc[1],x1,y1)+10) {
+          let A =[falak_pontjai[falak[j][0]][0],falak_pontjai[falak[j][0]][1]]; //mettol x,y
+          let B = [falak_pontjai[falak[j][1]][0],falak_pontjai[falak[j][1]][1]]; // meddig x,y
+          if (euklidesziTav(A[0],A[1],B[0],B[1]) >= euklidesziTav(mecc[0],mecc[1],A[0],A[1])+euklidesziTav(mecc[0],mecc[1],B[0],B[1])-5 && euklidesziTav(A[0],A[1],B[0],B[1]) <= euklidesziTav(mecc[0],mecc[1],A[0],A[1])+euklidesziTav(mecc[0],mecc[1],B[0],B[1])+5) {
+            sv.push(mecc);
+            sv2.push(euklidesziTav(mecc[0],mecc[1],x1,y1));
+            
+          }
+        }
+      }
+    }
 
-/*
-----------egyenes egyelnet
+    var abc2 = sv[sv2.indexOf(Math.min(...sv2))];
+    if (abc2 != undefined) {
+      mePontjai.push([Math.round(abc2[0]),Math.round(abc2[1])]);
+    }
 
-if (true) {
-      var vektor = [y1-x1,y2-x2]
-      console.log(vektor)
-      var normalVektor = [vektor[1],vektor[0]*(-1)]
-      console.log(normalVektor)
+    //sv2.push()
+  }
+    //console.log(sv)
+    //console.log(abc2)
+    
   
-      var jo_eredmeny = (normalVektor[0]*x1) + (normalVektor[1]*x2)
-      var p_eredmeny = (normalVektor[0]*playerX) + (normalVektor[1]*playerY)
-      console.log(jo_eredmeny)
-      console.log(p_eredmeny)
-      
+}
+function EgyenletRendszer(abc2,abc1) {
+  let y = (abc2[2]*abc1[0]-abc2[0]*abc1[2]) / (abc1[0]*abc2[1] - abc2[0]*abc1[1]);
+  let x = (abc1[2]-abc1[1]*y)/abc1[0];
+  if (x>=50 && x<= 1150 && y>=30 && y<=650) {
+    return [x,y];
+  }
+  return -1;
+}
+function FalakEgyenesEgyenlete() {
+  var egyenesek = [];
+  for (let i = 0; i < falak.length; i++) {
+    let A =[falak_pontjai[falak[i][0]][0],falak_pontjai[falak[i][0]][1]]; //mettol x,y
+    let B = [falak_pontjai[falak[i][1]][0],falak_pontjai[falak[i][1]][1]]; // meddig x,y
+    let irany_vektor = IranyVektor(B[0],B[1],A[0],A[1]);
+    let normal_vektor = NormalVektor(irany_vektor);
+    var abc = EgyenesEgyenlete(normal_vektor,A);
+    egyenesek.push(abc);
+  }
+  return egyenesek;
+}
 
-*/
+function IranyVektor(x1,y1,x2,y2) {
+  return [x1-x2,y1-y2];
+}
+function NormalVektor(iv) {
+  return [iv[1],iv[0]*(-1)];
+}
+function EgyenesEgyenlete(nv,A){
+  var a = nv[0];
+  var b = nv[1];
+  var c = a*A[0] + b*A[1];
+  return [a,b,c];
+}
+
+
